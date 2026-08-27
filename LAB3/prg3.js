2eecab3311bfb5a6fe09b8819bc8ebfa399647a5
Anuj Gupta <anujgupta6889@gmail.com>
2026-08-27 11:11:55 +0530

    upgrade

--- LAB3/prg3.js
+++ LAB3/prg3.js
@@ -1,13 +1,10 @@
-import http from 'http';
+import http from "http"
 
-const server= http.createServer((req, res)=>{
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { " content-type": "text/html"});
     
-    res.end("<h1> hello client part 3 </h1>");
-    console.log('Server hit by client');
+    res.end("<h2> Hello Client </h2>");
+     console.log("Server hit ");
 });
 
-server.listen(4444, ()=>{
-    console.log('Server is listening on port 4444');
-});
+server.listen(4444, ()=> console.log("Server is running ................"));

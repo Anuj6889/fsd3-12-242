3a0d2899c0f0bd8d3ab6c5b73fc4a6db9344e7bd
Anuj Gupta <anujgupta6889@gmail.com>
2026-09-10 11:21:43 +0530

    Done Changes

--- LAB3/prg6.js
+++ LAB3/prg6.js
@@ -1,8 +1,31 @@
-import http from 'http'
+import { createReadStream } from "fs";
+import http from "http";
 
-const server = http.createServer((req,res)=>
-{
+const server = http.createServer((req, res) => {
+    res.setHeader("content-type","text/html");
 
-})
+  if (req.url === "/"){
+    res.write("<h1>Home Page</h1>");
+    res.end(`
+        <a href= "/product">Product</a>
+        <a href ="/contact">Contact Us</a>
+        `);
+  }
+  else if (req.url === "/product") {
 
-server.listen(3000,()=>console.log('prg6 is running...'))
+    const stream = createReadStream("product.html", { encoding: "utf-8" });
+    stream.pipe(res);
+  }
+  else if (req.url === "/contact") {
+
+    const stream = createReadStream("contact.html", { encoding: "utf-8" });
+    stream.pipe(res);
+
+    
+  } else {
+    res.statusCode = 404;
+    res.end("Not found");
+  }
+});
+
+server.listen(3000, () => console.log("prg6 is running..."));
